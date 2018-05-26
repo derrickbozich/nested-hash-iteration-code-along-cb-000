4da7ec355cf39731601082de00e1433bb33f7aea
derrick b <dbozich1@gmail.com>
2018-05-26 22:25:44 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |name, data|
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
-        data.delete_if{|e| e == "strawberry"}
+        value.delete_if {|e| e == "strawberry"}
       end
     end
   end
